6e7a5ec458c8209e1d12f6363a53231bbf76a526
HyonNeo <128829684+HyonNeo@users.noreply.github.com>
2023-09-13 16:09:43 -0400

    controllers done

--- models/User.js
+++ models/User.js
@@ -12,7 +12,6 @@ const UsersSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      // use REGEX to validate correct email
       match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
   },
   thoughts: [{
@@ -33,13 +32,10 @@ const UsersSchema = new Schema(
   }
 )
 
-// get total count of friends
 UsersSchema.virtual('friendCount').get(function() {
   return this.friends.length;
 })
 
-// create the Users model using the Users Schema
 const Users = model('Users', UsersSchema);
 
-// Export Users module
 module.exports = User;
